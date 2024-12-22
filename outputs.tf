64236dfd918a165162e8fdec936982a4f3cb8f18
Sarabjeet Singh <sarabjeet.singh@brainridgeconsulting.com>
2024-12-22 14:16:42 -0500

    Revert the code

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
+
 ################################################################################
 # Cluster
 ################################################################################
@@ -155,7 +156,7 @@ output "service_security_group_id" {
 # Standalone Task Definition (w/o Service)
 ################################################################################
 
-/*output "task_definition_run_task_command" {
+output "task_definition_run_task_command" {
   description = "awscli command to run the standalone task"
   value       = <<EOT
     aws ecs run-task --cluster ${module.ecs_cluster.name} \
@@ -163,4 +164,4 @@ output "service_security_group_id" {
       --network-configuration "awsvpcConfiguration={subnets=[${join(",", module.vpc.private_subnets)}],securityGroups=[${module.ecs_task_definition.security_group_id}]}" \
       --region ${local.region}
   EOT
-}*/
+}
