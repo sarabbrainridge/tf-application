cde3a66b0f1335bd49456356acc20f5acbf2367b
Sarabjeet Singh <sarabjeet.singh@brainridgeconsulting.com>
2024-12-22 11:11:56 -0500

    Updated the ecr repository

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ module "ecs_service" {
       cpu       = 512
       memory    = 1024
       essential = true
-      image     = "public.ecr.aws/aws-containers/ecsdemo-frontend:776fd50"
+      image     = "864899849560.dkr.ecr.ca-central-1.amazonaws.com/craftcms:craftcms-package-8.4-latest"
       port_mappings = [
         {
           name          = local.container_name
