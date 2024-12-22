373d0a1d7c2ccc358bbaab68885e7ece33c964f9
Sarabjeet Singh <sarabjeet.singh@brainridgeconsulting.com>
2024-12-22 14:52:43 -0500

    Updated the VPC cidr

--- variables.tf
+++ variables.tf
@@ -6,3 +6,13 @@ variable "region_short_name" {
 variable "env" {
   default = "lab"
 }
+
+variable "vpc_id" {
+  default = "vpc-0d918cd659e10ed48"
+}
+
+variable "subnet_ids" {
+  type = list(string)
+  //default = ["subnet-0f146bbb3a2ef15ab","subnet-005660fe40bce333e","subnet-084cbb2cbe9a38268","subnet-095163acd49f50bef","subnet-0064d63862535d237","subnet-09615ab32c0f4fbc0"]
+  default = ["subnet-0f146bbb3a2ef15ab","subnet-005660fe40bce333e"]
+}
